9aa536825a3400fb762a1ab2a837ec6da6a86b0d
bivekk51 <nepaldynasty08@gmail.com>
2025-01-22 22:20:42 +0545

    debugs ffs

--- src/components/VideoRecorder.jsx
+++ src/components/VideoRecorder.jsx
@@ -42,7 +42,7 @@ const VideoRecorder = () => {
       formData.append("video", videoBlob, "capture.webm");
     
       try {
-        const response = await fetch("http://127.0.0.1:5000/upload", {
+        const response = await fetch("http://192.168.1.78:5000/upload", {
           method: "POST",
           body: formData,
           headers: {
