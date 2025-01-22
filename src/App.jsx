de449dca8d2e46ba9e0fd29e4e822b886de6b24c
bivekk51 <nepaldynasty08@gmail.com>
2025-01-22 22:43:22 +0545

    so much changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import VideoRecorder from './components/VideoRecorder'
-import VideoUploader from './components/VideoUploader'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import Navbar from './components/NavBar'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import VideoRecorder from './components/VideoRecorder';
+import VideoUploader from './components/VideoUploader';
 
-
+// About page component
 const About = () => (
   <div className="flex flex-col items-center mt-8">
     <h2 className="text-3xl font-bold mb-4">About</h2>
@@ -14,20 +14,32 @@ const About = () => (
     </p>
   </div>
 );
+
 const App = () => {
+  // Registering the service worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed: ', error);
+      });
+  }
+
   return (
     <>
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<VideoRecorder/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/upload" element={<VideoUploader />} />
-      </Routes>
-    </Router>
-      
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<VideoRecorder />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/upload" element={<VideoUploader />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
