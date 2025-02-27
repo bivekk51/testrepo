375c5b50564411b47dfe39e5e5b2f9f8afc9e50d
bivekk51 <nepaldynasty08@gmail.com>
2025-02-27 22:41:52 +0545

    push

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white px-6 py-3 flex justify-between items-center">
-      <h1>Violence Detector</h1>
+      <div>
+        <NavLink to="/" ><h1 className='hover:underline text-2xl font-semibold' >Violence Detector</h1></NavLink>
+      </div>
+      <div className='flex gap-8'>
+      <NavLink to="/" ><h1 className='hover:underline text-lg'>Home</h1></NavLink>
+      <NavLink to="/technicaldetails" ><h1 className='hover:underline text-lg'>Technical Details</h1></NavLink>
+
+      </div>
     </nav>
   );
 };
